842668bc480f83a2b1193b8e6b3ef8ece2ab5410
Bob Steers <kargathia@hotmail.com>
2018-07-04 16:13:16 +0200

    explicitly use home directory

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,8 +3,9 @@
 docker run \
     --rm \
     --detach \
-    --volume $(pwd):/workdir \
-    --workdir /workdir \
+    --name docker-compose-ui \
+    --volume /home/${USER}:/home/${USER} \
+    --workdir /home/${USER} \
     --publish 5000:5000 \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     brewblox/docker-compose-ui
