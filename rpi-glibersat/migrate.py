64ad345f6a070f3f0b0a0ece60fd5266eac23a61
Bob Steers <steers.bob@gmail.com>
2018-12-06 17:41:46 +0100

    make migrate script compatible python <3.6

--- rpi-glibersat/migrate.py
+++ rpi-glibersat/migrate.py
@@ -11,17 +11,17 @@ def main(filename):
         content = json.load(f)
 
     for db_name, db_content in content.items():
-        print(f'Creating database "{db_name}"')
-        requests.put(f'{HOST}/{db_name}')
+        print('Creating database "{}"'.format(db_name))
+        requests.put([HOST, db_name].join('/'))
 
         for obj in db_content:
             obj['_id'] = obj['id']
             del obj['id']
 
         docs = {'docs': [obj for obj in db_content]}
-        print(f'Filling database "{db_name}" with {len(db_content)} items')
+        print('Filling database "{}" with {} items'.format(db_name, len(db_content)))
 
-        resp = requests.post(f'{HOST}/{db_name}/_bulk_docs', json=docs)
+        resp = requests.post('{}/{}/_bulk_docs'.format(HOST, db_name), json=docs)
         print(resp.json())
 
 
