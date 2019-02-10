f722c7354282b5b07e65a9b1629542982bf87e84
Bob Steers <steers.bob@gmail.com>
2019-02-10 12:33:53 +0100

    display output points not score

--- downsample-viz/viz.py
+++ downsample-viz/viz.py
@@ -14,58 +14,63 @@ def calc(i):
     return max(target, i) / min(target, i)
 
 
-count = [i for i in range(target, 1000000, 100)]
+count = [i for i in range(target, 500000, 100)]
+combined_points = [
+    1,
+    12,  # downsample 1m
+    (12*10),  # downsample_10m
+    (12*10*6),  # downsample_1h
+    (12*10*6*6),  # downsample_6h
+]
+results = [[val // cmb for val in count] for cmb in combined_points]
 
 # Calculate score for each downsampling group
 # Floor divide i to simulate the grouping behavior of downsampling
 # Note: this is assumes a perfectly spaced write interval in the realtime dataset
 # Production values indicate there's a significant offset
-scores = [
-    [calc(i) for i in count],
-    [calc(i//12) for i in count],  # downsample 1m
-    [calc(i//(12*10)) for i in count],  # downsample_10m
-    [calc(i//(12*10*6)) for i in count],  # downsample_1h
-    [calc(i//(12*10*6*6)) for i in count],  # downsample_6h
-]
-
-# Comment out this for loop to view full range of data
-# We're deleting all data from non-chosen downsampling sets to improve clarity
 for vidx in range(len(count)):
-    slice = [score[vidx] for score in scores]
-    best_score = min([v for v in slice if v is not None])
+    slice = [result[vidx] for result in results]
+    scores = [calc(res) for res in slice]
+    best_score = min([s for s in scores if s is not None])
 
-    for sidx, score in enumerate(scores):
-        if slice[sidx] != best_score:
-            score[vidx] = None
+    for sidx, result in enumerate(results):
+        if scores[sidx] != best_score:
+            result[vidx] = None
 
 traces = [
     go.Scatter(
         x=count,
-        y=scores[0],
+        y=[target]*len(count),
+        mode='lines',
+        name='approx_points',
+    ),
+    go.Scatter(
+        x=count,
+        y=results[0],
         mode='lines',
         name='realtime',
     ),
     go.Scatter(
         x=count,
-        y=scores[1],
+        y=results[1],
         mode='lines',
         name='downsample_1m',
     ),
     go.Scatter(
         x=count,
-        y=scores[2],
+        y=results[2],
         mode='lines',
         name='downsample_10m',
     ),
     go.Scatter(
         x=count,
-        y=scores[3],
+        y=results[3],
         mode='lines',
         name='downsample_1h',
     ),
     go.Scatter(
         x=count,
-        y=scores[4],
+        y=results[4],
         mode='lines',
         name='downsample_6h',
     )
